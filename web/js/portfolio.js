ec047571914e9ec77ad651f22123655893bc13a9
Tan Xiang <tx-cary@163.com>
2018-09-14 10:06:16 +0000

    Regrouped the frontend data. Only exchange necessary data with backend. Most of data should be computed in front-end.

--- web/js/portfolio.js
+++ web/js/portfolio.js
@@ -72,16 +72,6 @@ var app = new Vue({
 			}
 			xhr.send(str);
 		},
-		keyToName: function(key) {
-			if(key===undefined){
-				return "";
-			}
-			if(this.nameOfKeys[key]===undefined) {
-				return key;
-			}else{
-				return this.nameOfKeys[key];
-			}
-		},
 		setCommand: function(cmd, id) {
 			this.command.pendingId = undefined;
 			this.command.pendingAmount = undefined;
@@ -126,21 +116,21 @@ var app = new Vue({
 		userName: "",
 		userPasswd: "",
 		isLogined: false,
-		nameOfKeys: {
-			refrate: "贴现率",
-			hkExchangeRate: "港币汇率",
-		},
-		statistics: {
-			refrate: 4.9,
-			hkExchangeRate: 0.8432,
-			hs300: 3225,
-			stockCapitalization: 20000,
-			hkCapitalization: 20000,
-			cash: 20000,
-			totalCapitalization: 60000,
-			estimatedCash: 20000,
-			totalProfitRate: 100,
-			yearProfitRate: -10,
+		statisticsView: {
+			editing: false,
+			shownTagsAsEditable: [
+				{key:'cash', displayAs:'现金'}, 
+			],
+			hidenTagsAsEditable: [
+				{key: 'virtualShare', displayAs:'组合份额'}, 
+				{key: 'virtualValue', displayAs:'组合净值'}, 
+				{key: 'virtualValueLYR', displayAs:'组合去年净值'},
+			],
+			shownTagsAsReadonly: [
+				{key:'stockCapitalization', displayAs:'A股市值'}, 
+				{key:'hkCapitalization', displayAs:'港股市值'}, 
+				{key:'totalCapitalization', displayAs:'总市值'}, 
+			],
 		},
 		command: {
 			pendingCommand: "",
@@ -152,12 +142,59 @@ var app = new Vue({
 			cmdNeedPrice: false,
 			cmdNeedSubmit: false,
 		},
+/*
+ * securejson = {
+ * 	statistics: {cash,virtualShare,virtualValue,virtualValueLYR},
+ * 	portfolio: [
+ * 		{id,share,score,unit},
+ * 		{...},
+ * 	]
+ * }
+ */		
+		statistics: {
+			//Configured
+			cash: 20000,
+			virtualShare: 100,
+			virtualValue: 2.1410,
+			virtualValueLYR: 5,
+			//fetched
+			refrate: 4.9,
+			hkExchangeRate: 0.8432,
+			hs300: 3225,
+			hs300LYR: 4030,
+			stockTrend: 100,
+			hkTrend: 100,
+			//Computed
+			stockCapitalization: 20000,
+			hkCapitalization: 20000,
+			totalCapitalization: 60000,
+			estimatedCash: 20000,
+			totalProfitRate: 100,
+			yearProfitRate: -10,
+		},
 		portfolio: [
-			{mark:"+", id: "600519", name: "贵州茅台", price: 646.0, share: 200, trade: 0, capitalization: 129200, level: 9.2, lowlevel: 11.1, uplevel:12.1, marketlevel: 14},
+			{
+				//configured
+				id: "600519", 
+				share: 200, 
+				score: 100,
+				unit: 100,
+				discount: 1.00,
+				//fetched
+				name: "贵州茅台", 
+				price: 646.0, 
+				//computed
+				mark:"+", 
+				trade: 0, 
+				capitalization: 129200, 
+				level: 9.2, 
+				lowlevel: 11.1, 
+				uplevel:12.1, 
+				marketlevel: 14,
+			},
 		],
 	},
 });
-
 
 function setCookie(space,name,value,exdays){
     var d = new Date();
